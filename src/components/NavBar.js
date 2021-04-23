c40a713d0268c2f59106e0030f92cd1779b483e3
Luke Thinnes <lukethinnes@yahoo.com>
2021-04-23 12:47:29 -0600

    Added social buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
     return (
-        <header className='bg-blue-900'>
+        <header className='bg-gray-900'>
             <div className='container mx-auto flex justify-between'>
                 <nav className='flex'>
                     <NavLink 
@@ -15,7 +16,7 @@ export default function NavBar() {
                         items-center 
                         py-8 px-3 
                         mr-4 
-                        text-red-100
+                        text-white
                         hover:text-green
                         text-4xl
                         font-bold
@@ -26,41 +27,50 @@ export default function NavBar() {
                     </NavLink>
                     <NavLink to='/project'
                     className='
-                        inline-flex 
-                        items-center 
-                        py-3 px-3 
-                        my-6 
-                        text-red-100
-                        rounded
-                        hover:text-green-800
-                    '>
+                    inline-flex 
+                    items-center 
+                    py-3 px-3 
+                    my-6 
+                    text-white
+                    rounded
+                    hover:text-green-900'
+                    activeClassName='text-red-100 bg-gray-400'
+                    >
                         Projects
                     </NavLink>
                     <NavLink to='/post'
+                    activeClassName='text-red-100 bg-gray-400'
                     className='
-                        inline-flex 
-                        items-center 
-                        py-3 px-3 
-                        my-6 
-                        text-red-100
-                        rounded
-                        hover:text-green-800
+                    inline-flex 
+                    items-center 
+                    py-3 px-3 
+                    my-6 
+                    text-white
+                    rounded
+                    hover:text-green-900
+                    
                     '>
                         Blog Posts
                     </NavLink>
                     <NavLink to='/about'
+                    activeClassName='text-red-100 bg-gray-400'
                     className='
                         inline-flex 
                         items-center 
                         py-3 px-3 
                         my-6 
-                        text-red-100
+                        text-white
                         rounded
-                        hover:text-green-800
+                        hover:text-green-900
                     '>
                         About
                     </NavLink>
                 </nav>
+                <div>
+                    <SocialIcon url='https://github.com/lukethinnes' className='mr-4' target='_blank' fgColor='#fff' style={{ height: 35, width: 35 }} />
+                    <SocialIcon url='https://www.linkedin.com/in/lucas-thinnes/' className='mr-4' target='_blank' fgColor='#fff' style={{ height: 35, width: 35 }} />
+                    <SocialIcon url='https://lucasthinnes.medium.com/' className='mr-4' target='_blank' fgColor='#fff' style={{ height: 35, width: 35 }} />
+                </div>
             </div>
         </header>
     )
