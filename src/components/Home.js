4fd7650c8bdff6351920f7c0675a8d52918633f6
Luke Thinnes <lukethinnes@yahoo.com>
2021-04-26 11:13:50 -0600

    Updated styling elements.

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
     return (
     <main>
         <img src={image} alt='background'className='absolute object-cover w-full h-full' />
-        <section>
-            <h1>Welcome to Lucas Thinnes' portfolio.</h1>
+        <section className='relative flex justify-center min-h-screen pt-12 lg: pt-64'>
+            <h1 className='text-6xl text-white greeting font-bold lg:leading-snug home-name'>Welcome! I am Lucas.</h1>
         </section>
     </main>
         )
