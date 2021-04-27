acbfb283d9a56c5fddacc53a8fffe32d60f58b37
Luke Thinnes <lukethinnes@yahoo.com>
2021-04-26 20:52:15 -0600

    Configured post page.

--- src/client.js
+++ src/client.js
@@ -2,5 +2,6 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
     projectId: '4p1njx99',
-    dataset: 'production'
+    dataset: 'production',
+    useCdn: true
 })
--- src/components/Post.js
+++ src/components/Post.js
@@ -17,8 +17,7 @@ export default function Post() {
                     },
                     alt
                 }
-            }`)
-            .then((data) => setPost(data))
+            }`).then((data) => setPost(data))
             .catch(console.error)
     }, [])
 
@@ -28,21 +27,25 @@ export default function Post() {
                 <h1 className='text 5xl flex justify-center cursive'>Blog Posts</h1>
                 <h2 className='text-lg text-gray-600 flex justify-center mb-12'>Here is a collection of my technical blog posts.</h2>
                 <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-                    {postData && postData.map((post, index) => (
-                    <article>
-                        <Link to={'/post/' + post.slug.current} key={post.slug.curent}>
-                        <span className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400' key={index}>
-                            <img 
-                                src={post.mainImage.asset.url}
-                                alt={post.mainImage.alt}
-                                className='w-full h-full rounder-r object-cover absolute'
-                            />
-                            <span className='bloack relative h-full flex justify-end items-end pr-4 pb-4'>
-                                <h3 className='text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacite-75 rounded'>{post.title}</h3>
-                            </span>
-                        </span>
-                        </Link>
-                    </article>
+                    {postData && 
+                        postData.map((post, index) => (
+                            <article>
+                                <Link to={'/post/' + post.slug.current} key={post.slug.curent}>
+                                <span 
+                                className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-100' 
+                                key={index}
+                                >
+                                    <img 
+                                        src={post.mainImage.asset.url}
+                                        alt={post.mainImage.alt}
+                                        className='w-full h-full rounder-r object-cover absolute'
+                                    />
+                                    <span className='bloack relative h-full flex justify-end items-end pr-4 pb-4'>
+                                        <h3 className='text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacite-75 rounded'>{post.title}</h3>
+                                    </span>
+                                </span>
+                            </Link>
+                        </article>
                     ))}
                 </div>
 
