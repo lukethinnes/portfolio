adb8d9c11e2826dca2f979ef6557ec62aba671da
Luke Thinnes <lukethinnes@yahoo.com>
2021-04-27 20:45:11 -0600

    Changed header and browser icon.

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
     return (
     <main>
         <img src={image} alt='background'className='absolute object-cover w-full h-full' />
-        <section className='relative flex justify-center min-h-screen pt-12 lg: pt-64'>
-            <h1 className='text-6xl text-white greeting font-bold lg:leading-snug home-name
+        <section className='relative flex justify-center min-h-screen pt-12 lg: pt-60'>
+            <h1 className='text-5xl text-white greeting font-bold lg:leading-snug home-name
             text-shadow: 18px 18px 18px rgb(200 200 200 / 88%), 18 18 18px rgb(155 203 206 / 88%)'>Welcome! I am Lucas.</h1>
         </section>
     </main>
